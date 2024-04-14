068ab82750b5635844b91605ab9dd18332351ff3
Tokyo772 <149683417+Tokyo772@users.noreply.github.com>
2024-04-14 15:27:48 +0000

    Deploying to gh-pages from @ Tokyo772/test-js@a3769a1bef41a8708c73a5264a6f458f25693b54 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,7 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import{c as r}from"./assets/styles-4240beca.js";function a(t){return t.map(({img:l,name:n,price:i,qty:o})=>`<li>
+      <img src="${l}" alt="${n}" />
+      <h2>${n}</h2>
+      <p>Quantity: ${o}</p>
+      <p>Total price: ${o*i} грн</p>
+    </li>`).join("")}const c=JSON.parse(localStorage.getItem(r.LS_PRODUCTS))??[],e={list:document.querySelector(".js-list"),bnt:document.querySelector(".js-clear")};c.length&&(e.bnt.hidden=!1);e.list.insertAdjacentHTML("beforeend",a(c));e.bnt.addEventListener("click",s);function s(t){localStorage.removeItem(r.LS_PRODUCTS),window.location.href="../index.html"}
 //# sourceMappingURL=commonHelpers.js.map
