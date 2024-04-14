b5d1c2f429a02054e6112ffb817dee232d913b09
Tokyo772 <149683417+Tokyo772@users.noreply.github.com>
2024-04-14 15:35:46 +0000

    Deploying to gh-pages from @ Tokyo772/test-js@cacdbedfa78a0753e9625a8dc38a4097aa5f7ebc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -3,5 +3,5 @@ import{c as r}from"./assets/styles-4240beca.js";function a(t){return t.map(({img
       <h2>${n}</h2>
       <p>Quantity: ${o}</p>
       <p>Total price: ${o*i} грн</p>
-    </li>`).join("")}const c=JSON.parse(localStorage.getItem(r.LS_PRODUCTS))??[],e={list:document.querySelector(".js-list"),bnt:document.querySelector(".js-clear")};c.length&&(e.bnt.hidden=!1);e.list.insertAdjacentHTML("beforeend",a(c));e.bnt.addEventListener("click",s);function s(t){localStorage.removeItem(r.LS_PRODUCTS),window.location.href="../index.html"}
+    </li>`).join("")}const c=JSON.parse(localStorage.getItem(r.LS_PRODUCTS))??[],e={list:document.querySelector(".js-list"),bnt:document.querySelector(".js-clear")};c.length&&(e.bnt.hidden=!1);e.list.insertAdjacentHTML("beforeend",a(c));e.bnt.addEventListener("click",s);function s(t){localStorage.removeItem(r.LS_PRODUCTS),window.location.href="./index.html"}
 //# sourceMappingURL=commonHelpers.js.map
